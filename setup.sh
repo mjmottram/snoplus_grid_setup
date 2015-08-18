a8e37f57b0f1560a41fa43abc72553bbaa992c23
Matt Mottram <mjmottram@gmail.com>
2015-08-18 16:45:04 +0100

    added voms info

--- setup.sh
+++ setup.sh
@@ -2,3 +2,8 @@
 
 # Setup a grid UI
 source /cvmfs/grid.cern.ch/emi3wn-latest/etc/profile.d/setup-wn-example.sh
+
+# Use the local SNO+ VOMS information
+CURDIR=$(dirname $(readlink -f $BASH_SOURCE))
+export VOMS_USERCONF=$CURDIR/voms_cfg/vomses
+export X509_VOMS_DIR=$CURDIR/voms_cfg/vomsdir
