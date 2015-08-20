e20b526fcd50e83874381e8f28c3e87adcb8078d
Matt Mottram <m.mottram@qmul.ac.uk>
2015-08-20 13:34:08 -0500

    added bdii and lfc info

--- setup.sh
+++ setup.sh
@@ -7,3 +7,7 @@ source /cvmfs/grid.cern.ch/emi3wn-latest/etc/profile.d/setup-wn-example.sh
 CURDIR=$(dirname $(readlink -f $BASH_SOURCE))
 export VOMS_USERCONF=$CURDIR/voms_cfg/vomses
 export X509_VOMS_DIR=$CURDIR/voms_cfg/vomsdir
+
+# Set correct grid access points
+export LFC_HOST=lfc.gridpp.rl.ac.uk
+export LCG_GFAL_INFOSYS=lcgbdii.gridpp.rl.ac.uk:2170
